c39a48230ec3d54ceaa6b47657adaa6a0f4538b9
bhav380 <bhaveshrathour380@gmail.com>
2023-08-25 16:42:43 +0530

    second commit

---
+++ favorites/favorites.js
@@ -0,0 +1,215 @@
+(function () {
+
+    const API_KEY = "5d3bf326";
+
+    if (localStorage.getItem('favMoviesImdbid') == null) {                   // if favMoviesImdbid in local storage is null create favMoviesImdbid which will store imdbid of movies added to favorites
+        localStorage.setItem('favMoviesImdbid', JSON.stringify([]));
+    }
+
+    //_________________________________________________Fetch and append Fav Movies to #fav-movies__________________________________________________________________
+    function fillDataInCard(cardClone, movie) {
+
+        const movieCard = cardClone.querySelector('.favorite-movie-card');
+        const poster = cardClone.querySelector('.favorite-movie-card div img');
+        const title = cardClone.querySelector('.title');
+        const year = cardClone.querySelector('.year');
+        const type = cardClone.querySelector('.type');
+        const watchBtn = cardClone.querySelector('.watch');
+        const deleteBtn = cardClone.querySelector('.delete i');
+
+        movieCard.dataset.idimdb = movie.imdbID;
+        watchBtn.dataset.imdbid = movie.imdbID;
+        deleteBtn.dataset.imdbid = movie.imdbID;
+        year.innerHTML = movie.Year;
+        type.innerHTML = movie.Type;
+
+
+        if(movie.Poster=='N/A'){
+            poster.src = '../images/imgNotAvailable.jpg';
+        }else{
+            poster.src = movie.Poster;
+        }
+
+        if (movie.Title.length > 37) {       // if title length is greater than 37 , 37 characters of fetched title will be inserted inside title element
+            title.innerHTML = `${movie.Title.slice(0, 36)}...`
+        } else {
+            title.innerHTML = movie.Title;
+        }
+    }
+
+    async function fetchMovieData(movieImdbid) {
+
+        const templateFavMovie = document.getElementById('template-fav-movie');       
+        const favMovies = document.getElementById('fav-movies');
+        let url = `http://www.omdbapi.com/?i=${movieImdbid}&apiKey=${API_KEY}`;
+
+        const res = await fetch(`${url}`);
+        const data = await res.json();
+
+        const cardClone = templateFavMovie.content.cloneNode('true');
+        fillDataInCard(cardClone, data);                // fills data of fetched movie to cardClone
+        favMovies.appendChild(cardClone);               // appends cardClone to #fav-movies (favMovies)
+    }
+
+    function appendFavMovies() {
+        const favMovies = document.getElementById('fav-movies');
+        favMovies.innerHTML='';
+        let imdbidArray = JSON.parse(localStorage.getItem('favMoviesImdbid'));
+        imdbidArray.forEach((id) => {
+            fetchMovieData(id);              //fetched movie from api using imdbid and adds it to #fav-movies
+        });
+    }
+
+
+    //_________________________________________Add and Remove From Favorites (using start icon in movie cards)_____________________________________
+    function addToFavorites(imdbid) {
+
+        let imdbidArray = JSON.parse(localStorage.getItem('favMoviesImdbid'));
+        imdbidArray.push(imdbid);
+        // console.log(imdbidArray);
+        localStorage.setItem('favMoviesImdbid', JSON.stringify(imdbidArray));
+        fetchMovieData(imdbid);
+    }
+
+    function removeFromFavorites(imdbid) {
+
+        let imdbidArray = JSON.parse(localStorage.getItem('favMoviesImdbid'));
+        imdbidArray = imdbidArray.filter((id) => {        // removes imdbid from imdbidArray
+            if (id != imdbid) {
+                return id;
+            }
+        });
+
+        let favMoviesBox = document.querySelector('#fav-movies')
+        let target = favMoviesBox.querySelectorAll(`[data-idimdb="${imdbid}"]`);
+        target[0].remove();      // removes movie from #fav-movies box
+        localStorage.setItem('favMoviesImdbid', JSON.stringify(imdbidArray));  // saves updated imdbidArray in localStorage
+    }
+
+    function addAndRemoveFavorites() {
+
+        let imdbCloneContainerMain = document.querySelector('#imdb-clone-container main');
+        imdbCloneContainerMain.addEventListener('click', (event) => {
+
+            if (event.target.className == 'fa-solid fa-star') {    // if star icon is clicked
+
+                event.target.parentElement.style.outline = 'none';    // removing onclick border property of button which contains fa-star icon
+
+                if (event.target.dataset.isfav == 'false' && confirm('Add to favories ? ')) {    // if movie is not present in favorites
+                    event.target.dataset.isfav = 'true';          
+                    event.target.style.color = 'red';                            // color of start icon is set to red
+                    event.target.style.fontSize = '1.2rem';
+                    addToFavorites(event.target.dataset.imdbid);                   // saves imdbid of movie in localstorage and adds it to #fav-movies box
+
+                } else if(confirm('Remove from favorites ?')) {                 // if movie is present in favorites , deletes imdbid of this movie from localStorage and removes this movie from #fav-movies box 
+                    removeFromFavorites(event.target.dataset.imdbid);
+                    event.target.dataset.isfav = 'false';
+                    event.target.style.color = '#d54b90';                       // color of star icon is set to light pink
+                    event.target.style.fontSize = '1rem'
+                }
+            }
+        });
+    }
+
+    // __________________________________display favorite page_________________________________
+    function favoriteMoviePage() {
+
+        let openFavPageButton = document.querySelector('#nav-items>a:nth-child(3)');
+        let closeFavPage = document.querySelector('#close-favorites');
+
+        let favoritesContainer = document.getElementById('favorites-container');
+        let main = document.querySelector('#imdb-clone-container main');
+        openFavPageButton.addEventListener('click', (e) => {
+            e.preventDefault();
+            favoritesContainer.style.display = 'flex';
+            main.style.opacity = '0.65';
+
+        });
+
+        closeFavPage.onclick = () => {
+            favoritesContainer.style.display = 'none';
+            main.style.opacity = '1';
+        }
+    }
+
+
+    // ________________________________________see movie Details (watch button linked to movie.html page) and Delete functionality using trash icon_______________________________________________
+
+    function seeMovieDetailsAndDelete() {
+        let favMovies = document.getElementById('fav-movies')
+
+        favMovies.addEventListener('click', (event) => {
+
+            if (event.target.className == 'watch') {                // on clicking watch button
+                localStorage.setItem("seeMovieImdbid", event.target.dataset.imdbid);   // seeMovieImdbid variable in localStorage is used by movie.js to show movie details, it stores imdbid of movie whose details users wants to watch
+                window.location.href = './movie/movie.html';                           // opens movie.html page
+            }
+
+            if (event.target.className == 'fa-solid fa-trash' && confirm('Remove From Favorites ?')) {  // on clicking delete button
+
+                removeFromFavorites(event.target.dataset.imdbid);            
+                imdbApp.getTopMovies();             // refreshes movies inside #top-movies-container in main (to change data attribute isFav of removed movie from true to false and fa-star-icon color (from red to light pink) )
+
+                let movieName = document.querySelector('#search div #movie-name');
+                let year = document.querySelector('#search div #year');
+                imdbApp.movieSuggestions(movieName,year);  // refreshes movie suggestions (changes isFav data attribute of removed movie to false)
+            }
+        });
+    }
+
+    function favorites() {
+
+        appendFavMovies();            // adds movies whose imdbid is present inside favMoviesImdbid (localStorage variable) in DOM
+
+        addAndRemoveFavorites();      // add and remove favortie movies (using fa-star icon) functionality inside movie cards present in main 
+
+        favoriteMoviePage();           // this function displays favMovies on clicking favorites link in navbar and adds close fav page functionality 
+
+        seeMovieDetailsAndDelete();   // links watch button of movies (inside #fav-movies) to movie.html page and adds delete movie functionality
+    }
+
+    //_____________________________________call to favorites()___________________________________________________________________________________
+
+    favorites();  
+
+})();
+
+
+
+
+
+    // function favoriteMoviePage() {
+
+
+    //     let openFavPageButton = document.querySelector('#nav-items>a:nth-child(3)');
+    //     let showFavPage = false;
+
+    //     openFavPageButton.addEventListener('click', (e) => {
+    //         e.preventDefault();
+    //         showFavPage = !showFavPage;
+
+    //         let favoritesContainer = document.getElementById('favorites-container');
+    //         let main = document.querySelector('#imdb-clone-container main');
+
+    //         if (showFavPage) {
+    //             favoritesContainer.style.display = 'flex';
+    //             main.style.opacity = '0.65';
+
+    //         } else {
+    //             favoritesContainer.style.display = 'none';
+    //             main.style.opacity = '1';
+    //         }
+
+    //     });
+
+    // }
+
+
+
+    
+        // let favimdbids = JSON.parse(localStorage.getItem('favMoviesImdbid'));
+        // favimdbids.filter((id) => {                         
+        //     if (movie.imdbID == id) {                   // if imdbid of this movie is present inside favMoviesImdbid array (present inside localStorage) delete buttons data-attribute isfav is set to true
+        //         deleteBtn.dataset.isfav = 'true';
+        //     }
+        // });
---
+++ script.js
@@ -0,0 +1,229 @@
+//REVEALING MODULE DESIGN PATTERN
+var imdbApp = (function () {
+
+    const API_KEY = "5d3bf326";
+    var seeMovieImdbId;   //  when watch button of any movie card is clicked , this variable will store imdbid of that movie ...then value of this variable is stored inside local storage ... which then is accessed by movie.js file to render movie details
+    var topMoviesImdbId = ["tt9362722", "tt15398776", "tt1517268", "tt16437278", "tt3107288", "tt6791350", "tt10366206", "tt1630029", "tt14444726", "tt11145118"]; // Movies having these imdbid will be shown inside Top Movies Container
+
+
+
+    //performs animation on header image (zoom outs the img when page loads)
+    function headerImgAnimation() {
+
+        let img = document.querySelector("#header-img-container img");
+        setTimeout(function () {
+            img.style.transform = "scale(1)"
+        });
+
+    }
+
+
+    // fills top-movies-container with movies whose imdbid is present inside topMoviesImdbId array
+    function getTopMovies() {
+        const movieCardsTemplate = document.getElementById('template-movie-card');
+        const moviesContainer = document.querySelector('#top-movies-container #movies-container');
+        moviesContainer.innerHTML = '';
+
+        topMoviesImdbId.forEach(async (id) => {
+
+            let url = `http://www.omdbapi.com/?i=${id}&apiKey=${API_KEY}`;
+            const res = await fetch(`${url}`);
+            const data = await res.json();
+
+            const cardClone = movieCardsTemplate.content.cloneNode(true);  //clones #template-movie-card
+            fillDataInCard(cardClone, data);
+            moviesContainer.append(cardClone);                             // appends cardClone to #movies-container
+        });
+    }
+
+
+    // on clicking watch button (inside movie card) movie.html page will open
+    function seeMovieDetails(event) {
+
+        if (event.target.className == 'btn') {
+            localStorage.setItem("seeMovieImdbid", event.target.dataset.imdbid);
+            event.target.href = './movie/movie.html';
+        }
+    }
+
+
+    //___________________________________________________________Search Movies______________________________________________________________
+
+    function fillDataInCard(cardClone, movie) {
+        const movieCard = cardClone.querySelector('.movie-card');
+        const poster = cardClone.querySelector('.movie-card .image img');
+        const title = cardClone.querySelector('.movie-title');
+        const year = cardClone.querySelector('.year');
+        const type = cardClone.querySelector('.type');
+        const watchBtn = cardClone.querySelector('.btn');
+        const favBtn = cardClone.querySelector('.add-to-favorites i');
+
+        movieCard.dataset.imdbid = movie.imdbID;
+        watchBtn.dataset.imdbid = movie.imdbID;
+        favBtn.dataset.imdbid = movie.imdbID;
+        title.innerHTML = movie.Title;;
+        year.innerHTML = movie.Year;
+        type.innerHTML = movie.Type;
+
+
+        if(movie.Poster=='N/A'){
+
+            poster.src = './images/imgNotAvailable.jpg';
+
+        }else{
+            poster.src = movie.Poster;
+        }
+
+        console.log(movie.Title);
+        console.log(movie.Poster);
+
+        let favimdbids = JSON.parse(localStorage.getItem('favMoviesImdbid')); // extracts imdbIds of movies inside favorties
+        favimdbids.filter((id) => {
+            if (movie.imdbID == id) {                // if movie is already added to favorites by user data-id attribute isFav will be set to true and addtoFav button will have red color (instead of pink)
+                favBtn.dataset.isfav = 'true';
+                favBtn.style.color = 'red';
+                favBtn.style.fontSize = '1.1rem';
+            }
+        });
+    }
+
+    function bindData(movies) {
+
+        const movieCardsContainer = document.getElementById("search-results");
+        const movieCardsTemplate = document.getElementById('template-movie-card');
+
+        movies.forEach((movie) => {
+            const cardClone = movieCardsTemplate.content.cloneNode(true);     //clones #template-movie-card
+            fillDataInCard(cardClone, movie);                                  //fills movie data inside cardClone
+            movieCardsContainer.appendChild(cardClone);                       // appends cardClone to #search-results (movieCardsContainer)
+        });
+    }
+
+    function loadMoreMovies(url, currentItem) {
+
+        let loadMoreBtn = document.querySelector('#load-more-button');
+        loadMoreBtn.style.display = 'block';
+        let pageNum = 2;                       //inital value of pageNum =2
+
+        //if load move button is clicked loads 6 more movies in DOM
+        loadMoreBtn.onclick = async () => {
+
+            let cards = document.querySelectorAll('#search-results .movie-card');
+            for (let i = currentItem; i < currentItem + 6 && i < cards.length; i++) {     
+                cards[i].style.display = 'flex';
+            }
+
+            currentItem += 6;
+
+            // if currentItem exceeds number of movies present inside #searchResults , again api call is made to fetch more movies ...(one api call gets 10 movies)
+            if (currentItem >= cards.length) {
+                let prevCardLength = cards.length;
+                if (await fetchMovies(`${url}&Page=${pageNum}`)) {           //Page = [pagenum] is passed as query in url (it gets movies from page = [pagenum] (ex-page=2 ,page=3 so on..) of api response )
+
+                    let updatedCards = document.querySelectorAll('#search-results .movie-card');    
+
+                    for (let i = prevCardLength; i < currentItem && i < updatedCards.length; i++) {    // loads remaining movies into DOM ( if less than 6 movies were loaded on clicking load button (this happens when there are no movies to load and we need to make api call for next page))
+
+                        updatedCards[i].style.display = 'flex';
+                    }
+
+                } else {
+                    loadMoreBtn.style.display = 'none';           // if there are no more pages to fetch movies loadMore Button display will be set to none
+                }
+                pageNum++;                                     
+            }
+        }
+    }
+
+    async function fetchMovies(url) {
+
+        const res = await fetch(`${url}`);
+        const data = await res.json();
+
+        if (data.Response == 'False') {
+            return false;                 // if fails to fetch movies , returns false
+        }
+        if (data.totalResults > 6) {
+            loadMoreMovies(url, 6);        //on clicking loadMore button 6 more movies are loaded in DOM
+        }
+        bindData(data.Search);            // fills fetched movies inside #search-results
+        return true;
+    }
+
+    async function movieSuggestions(movieName, year) {
+
+        const movieCardsContainer = document.getElementById("search-results");
+        movieCardsContainer.innerHTML = "";
+
+        let url = "http://www.omdbapi.com/?";
+        if (movieName.value != '') {                         //condition is executed if moviename feild is not left empty by user
+            url = `${url}&s=${movieName.value}`;
+        }
+
+        if (year.value != '') {                              //condition is executed if year feild is not left empty by user
+            url = `${url}&y=${year.value}`;
+        }
+
+        url = `${url}&apiKey=${API_KEY}`;
+
+        let searchResultsMsg = document.getElementById('search-results-msg');
+
+        if (await fetchMovies(url)) {              //fetches movies from obtained url , below code shows msg above search results depending on success of fetchMovies(url)
+
+            searchResultsMsg.innerHTML = `Search results for <span>${movieName.value}</span> : `;
+        } else {
+
+            searchResultsMsg.innerHTML = `No result found`;
+        }
+    }
+
+    function searchMovies() {
+
+        let movieName = document.querySelector('#search div #movie-name');
+        let year = document.querySelector('#search div #year');
+
+        //on pressing keys inside search movie, shows search results automatically (no need to click search button)
+
+        movieName.addEventListener('keyup', () => {
+            movieSuggestions(movieName, year)
+        }
+        );
+
+        year.addEventListener('keyup', () => {
+            movieSuggestions(movieName, year)
+        });
+    }
+
+
+    //________________________________________________window onload________________________________________________________
+
+
+    window.onload = () => {
+
+        headerImgAnimation();
+        getTopMovies();
+
+        let topMoviesContainer = document.getElementById('movies-container');
+        topMoviesContainer.addEventListener('click', function (event) {         //handling clicking of watch button inside movie cards
+            seeMovieDetails(event);
+        });
+
+        searchMovies();         //Search movie functionalities
+
+        let searchResults = document.querySelector("#search-results");
+        searchResults.addEventListener('click', function (event) {
+            seeMovieDetails(event);
+        });
+    }
+
+    return {
+        SeeMovieImdbId: seeMovieImdbId,
+        getTopMovies: getTopMovies,                 // exporting getTopMovies() and movieSuggestions() functions ----(used by favorite.js to toggle data-id attribute isFav of movie cards)
+        movieSuggestions: movieSuggestions
+    }
+
+})();
+
+
+
+
